0c0efa40371e0e8fff2984e59011a8b697eb1f7b
termisam005 <termi0004@gmail.com>
2021-08-29 20:09:48 +0900

    commit-0829

--- src/pages/Collabo.jsx
+++ src/pages/Collabo.jsx
@@ -1,28 +1,52 @@
 import { useState } from "react";
 import Header from "../components/header/Header";
-import PopComponent from "../components/common/PopComponent";
 
 export default function Collabo() {
-    const [popUps, setpopUps] = useState(false);
 
-    const openPop = () => {
-        setpopUps(true);
-    }
-    const closePop = () => {
-        setpopUps(false);
+    const [toggleState, setToggleState] = useState(1);
+
+    const toggleTab = (index) => {
+        setToggleState(index);
     }
 
     return (
         <div className="wrap">
             <Header />
             <div className="container">
-                <div className="contents">
-                    <h1>Collabo</h1>
 
-                    <button onClick={openPop}>팝업오픈</button>
-
-                    <PopComponent open={popUps} close={closePop} pop_txt={"팝업테스트"} />
+                <div className="dpflx">
+                    <div className={toggleState === 1 ? "tabs active_tabs" : "tabs"} onClick={() => toggleTab(1)}>
+                        Tab 1
+                    </div>
+                    <div className={toggleState === 2 ? "tabs active_tabs" : "tabs"} onClick={() => toggleTab(2)}>
+                        Tab 2
+                    </div>
+                    <div className={toggleState === 3 ? "tabs active_tabs" : "tabs"} onClick={() => toggleTab(3)}>
+                        Tab 3
+                    </div>
                 </div>
+
+                <div className="content_tab">
+                    <div className={toggleState === 1 ? "cont active_cont" : "cont"}>
+                        <h2>Cont1</h2>
+                        <p>
+                            1bsdfioiflkejfoiwefjofwofofjofjlksf;d' fklwefjweiofjieofe
+                        </p>
+                    </div>
+                    <div className={toggleState === 2 ? "cont active_cont" : "cont"}>
+                        <h2>Cont2</h2>
+                        <p>
+                            2bsdfioiflkejfoiwefjofwofofjofjlksf;d' fklwefjweiofjieofe
+                        </p>
+                    </div>
+                    <div className={toggleState === 3 ? "cont active_cont" : "cont"}>
+                        <h2>Cont3</h2>
+                        <p>
+                            3bsdfioiflkejfoiwefjofwofofjofjlksf;d' fklwefjweiofjieofe
+                        </p>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
